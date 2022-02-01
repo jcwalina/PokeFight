0025fc8718bb84bfe186bc0fdbabba3d28bb25b0
jcwalina <joannacwalinaa@gmail.com>
2022-02-01 18:22:28 +0100

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 require("dotenv").config();
 const pokemonRouter = require("./routes/pokemonRouter");
 
